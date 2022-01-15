9c592121fd0734d46c50f9cfa663aa96cd2716c3
Leonard <smartycope@gmail.com>
2022-01-15 15:32:08 -0700

    Standard Sync at Sat Jan 15 03:32:08 PM MST 2022 from leonard

--- src/MainWindow/_customActions.py
+++ src/MainWindow/_customActions.py
@@ -16,6 +16,7 @@ def autism(func, *args, **kwargs):
 todo('replace the unit solver with a search for equation box, given the units involved')
 todo('maybe make getting atoms exclusively in the variable boxes optional')
 todo('remove (1) from one in the prefix box')
+todo('add a prefix box to the solution unit')
 
 def addCustomFuncs(self):
     with open(customFuncsFile, 'r') as f:
@@ -35,7 +36,6 @@ def addCommonEqus(self):
         for item in json.load(f):
             if item['catagory'] not in self._commonEquCatagories.keys():
                 self._commonEquCatagories[item['catagory']] = self.menuCommonEquations.addMenu(item['catagory'])
-            debug(item['definition'])
             ihateeveryone = deepcopy(item['definition'])
             self._commonEquCatagories[item['catagory']].addAction(item['name'], autism(self.equationInput.setPlainText, ihateeveryone)) # lambda: self.equationInput.setPlainText(ihateeveryone))
 
